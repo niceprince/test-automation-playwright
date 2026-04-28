2d6b46627739a6d6b119eb8b09f6648a34ec78cb
Prince <prince@Princes-MacBook-Pro.local>
2026-04-28 17:49:15 +0530

    fixed issues

--- tests/autoSuggestDropdown.spec.ts
+++ tests/autoSuggestDropdown.spec.ts
@@ -3,13 +3,16 @@ import { test, expect, Locator } from "@playwright/test";
 
 test("Auto Suggest Dropdown Handling", async ({page}) => {
   await page.goto("https://www.flipkart.com/");
+
   await page.waitForTimeout(1000);
+  const loginPopupClose:Locator = await page.locator("div[tabindex='-1'] span[role='button']");
+  await loginPopupClose.click();
 
   await page.locator("input[name='q']").first().fill('smart');
+
   await page.waitForTimeout(2000);
 
   const options:Locator = page.locator('ul>li');
-
   const count:Number = await options.count();
 
   console.log('checking search count', await options.allTextContents());
@@ -17,7 +20,7 @@ test("Auto Suggest Dropdown Handling", async ({page}) => {
   for(let i = 0; i < count; i++){
     const searchText:string = await options.nth(i).textContent();
     console.log("Options datas.... => ", searchText)
-    if(searchText === 'smartphone'){
+    if(searchText?.trim()  === 'smartphone'){
       await options.nth(i).click();
       break;
     }
--- tests/hiddenBootstrapDropdown.spec.ts
+++ tests/hiddenBootstrapDropdown.spec.ts
@@ -6,10 +6,9 @@ test("Open source orange hrm live login flow", async ({page}) => {
   // Login steps
   await page.locator("input[name='username']").fill("Admin");
   await page.locator("input[name='password']").fill("admin123");
-
   await page.locator("button[type='submit']").click();
 
-  await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+  await expect(page).toHaveURL(/dashboard/);
 
   await page.locator("li.oxd-main-menu-item-wrapper > a.oxd-main-menu-item").getByText("PIM").click();
   //console.log(pimLink.getByText());
@@ -17,21 +16,20 @@ test("Open source orange hrm live login flow", async ({page}) => {
 
   await page.locator("form i").nth(2).click();
 
-  await page.waitForTimeout(2000);
+  const options = await page.locator("div[role='listbox'] span");
 
-  const options:Locator[] = await page.locator("div[role='listbox'] span");
+  await expect(options.first()).toBeVisible();
 
   const count:Number = await options.count();
-
   console.log("Dropdown items number count", await count);
   
   for(let i = 0; i < count; i++) {
     const text = await options.nth(i).innerText();
-    if(text === "Automaton Tester"){
+    if(text === ("Automaton Tester" || "Automation Tester")){
       await options.nth(i).click();
       break;
     }
   }
 
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(2000);
 })
